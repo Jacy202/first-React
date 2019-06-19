be6debddfe817f975edae999cfb4451f8d4897e2
roxilla <jacy202@yahoo.com>
2019-06-19 12:33:45 +0100

    first React

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import React, {component} from 'react';
+const Header = () =>{
+    return(
+    <h2>TODO_App</h2>
+    )
+}
+
+export default Header
---
+++ src/TodoItems.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+const TodoItems = () => {
+    return ( 
+        <h2> I render individual todo Items</h2>
+    )
+}
+
+export default TodoItems;
