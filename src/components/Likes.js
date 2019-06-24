08d76860ffd7f98607e1ec636363cbe995c980ee
roxilla <jacy202@yahoo.com>
2019-06-24 13:24:36 +0100

    completed

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -2,16 +2,33 @@ import React, {Component} from 'react'
 
 export class Likes extends Component {
     state = {
-        likes: 0
-    }
+        likes: 0,
+    };
+    increaseLikes = () => {
+        this.setState((prevState) => {
+            return {
+                likes: prevState.likes + 1
+            }
+        })
+    };
+    decreaseLikes =() => {
+        this.setState((prevState) => {
+            return{
+                likes: prevState.likes -1
+            };
+        });
+    };
+            resetLikes = () => {
+                this.setState ({likes: 0 })
+            };
     render () {
         return (
             <div>
                 <h1>welcome to my Likes App</h1>
                 <h3>likes: {this.state.likes}</h3>
-                <button>like</button>
-                <button>unlike</button>
-                <button>reset</button>
+                <button onClick ={this.increaseLikes}>like</button>
+                <button onClick = {this.decreaseLikes}>unlike</button>
+                <button onClick = {this.resetLikes}>reset</button>
             </div>
         )
     }
