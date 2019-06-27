f6277f0892908f747dc4791f29966c755dbf7b80
roxilla <jacy202@yahoo.com>
2019-06-27 16:58:31 +0100

    concluded storage

---
+++ src/components/LocalStorage.js
@@ -0,0 +1,29 @@
+import React,{Component} from "react";
+
+class LocalStorage extends Component {
+    render() {
+        const person = {
+            name: "chidera",
+            age: 16
+        }
+        //convert to json object
+        const personToJSON= JSON.stringify(person)
+
+        //save to local storage
+        localStorage.setItem("newPerson", personToJSON);
+        //retrieve from ls
+        const getPerson = localStorage.getItem("newPerson")
+        //convert to objectnewPerson
+        const personParse = JSON.parse(getPerson);
+        return(
+            <div>
+            <h3>The stringified version is</h3>
+            <p>{getPerson}</p>
+            <h4>The parsed version is:</h4>
+            <p>{personParse.name} and {personParse.age}</p>
+            </div>
+        )
+    }
+}
+
+export default LocalStorage;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import C from "./assignment1/C";
 import Todo from './components/Todo';
 import TodoItems from './components/TodoItems';
 import Likes from "./components/Likes"
+import LocalStorage from './components/LocalStorage';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Todo />, document.getElementById('root'));
+ReactDOM.render(<LocalStorage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
